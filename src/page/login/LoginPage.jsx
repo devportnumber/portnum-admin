5efbf3159bf0fe8eed12f492527e0113bee223fa
Hyebin-woo <dev.hyebin@gmail.com>
2024-10-11 16:06:46 +0900

    Modify: 로그인 아이디로 변경

--- src/page/login/LoginPage.jsx
+++ src/page/login/LoginPage.jsx
@@ -12,8 +12,9 @@ const LoginPage = () => {
   const [form] = Form.useForm()
   const values = Form.useWatch([], form)
   const [loginForm, setLoginForm] = useState({
-    email: 'hello2@gmail.com',
-    password: 'port8000!',
+    // email: 'hello2@gmail.com',
+    loginId: 'testWebin',
+    password: 'qwer1234!',
   })
   const navigate = useNavigate()
   const { login } = useAuth()
@@ -90,22 +91,22 @@ const LoginPage = () => {
         }}
       >
         <Form.Item
-          name="email"
+          name="loginId"
           // label="이메일"
           // rules={[{ required: true, message: '컨텐츠 명을 입력하세요!' }]}
         >
-          <Input placeholder="email" />
+          <Input placeholder="아이디" />
         </Form.Item>
         <Form.Item
           name="password"
           // label="비밀번호"
           // rules={[{ required: true, message: '컨텐츠 명을 입력하세요!' }]}
         >
-          <Input type="password" placeholder="Password" required />
+          <Input type="password" placeholder="비밀번호" required />
         </Form.Item>
         <Button btnText="로그인" htmlType={'submit'} onClick={handleSubmit} />
         <LinkGroup>
-          <LinkTxt to="/find-email">아이디 찾기</LinkTxt>
+          <LinkTxt to="/find-loginId">아이디 찾기</LinkTxt>
           <span>|</span>
           <LinkTxt to="/find-password"> 비밀번호 찾기 </LinkTxt>
           <span>|</span>
